8882b9e190e67d38a115826871441343c2a3539b
shivanikewat9691 <shivani.s@pixoatic.com>
2024-01-29 15:17:47 +0530

    footer changes and redirect new page

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :learning_platform
   get 'Advantage', to: 'learning_platform#Advantage',as: 'Advantage'
+  get 'terms', to: 'learning_platform#terms', as: "terms"
+  get 'privacy', to: 'learning_platform#privacy', as: "privacy"
+  get 'refund', to: 'learning_platform#refund', as: "refund"
+  get 'jobs', to: 'learning_platform#jobs', as: 'jobs'
+  get 'procast', to: 'learning_platform#procast', as: 'procast'
 end
